147bd90be052ddfa30bb41fba76d60aa33c6b5cc
GreatValour <oluwanifemivalour@outlook.com>
2024-02-04 09:45:10 -0500

    first commit

---
+++ wp-content/themes/hdtheme/js/menuEFB996ver=18.js
@@ -0,0 +1,116 @@
+/**
+ * HddesignMenu 
+ * 
+ * Horizontal Multilevel Menu with WP MegaMenu Support 
+ * 
+ * 3.0 | HD Designs
+ */
+
+(function( $ ){
+	"use strict";
+	
+	$.fn.hkdMenu = function( options ){
+
+		var menu = $(this);
+		
+		var defaults = {
+			addLast		: false,
+			arrows      : false,
+			delay       : 100,
+			hoverClass  : 'hover',
+			mobileInit	: 768,
+			responsive	: true
+		};
+		options = $.extend( defaults, options );
+		
+		
+		var init = function(){
+			
+			// add '.submenu' class
+			$( 'li:has(ul)', menu ).addClass( 'submenu' );
+			
+			// append mobile toggle button
+			$( 'li:has(ul)', menu ).append( '<span class="menu-toggle"></span>' );
+			
+			// add '.hkd-megamenu-parent' class
+			menu.children( 'li:has( ul.hkd-megamenu )' ).addClass( 'hkd-megamenu-parent' );	
+			
+			// add '.last-item' class
+			$( '.submenu ul li:last-child', menu ).addClass( 'last-item' );
+			
+			// add '.last' class
+			if( options.addLast ) {
+				$( '> li:last-child', menu ).addClass( 'last' )
+					.prev().addClass( 'last' );
+			}
+			
+			// appand submenu arrows
+			if( options.arrows ) {
+				$( 'li ul li:has(ul) > a', menu ).append( '<i class="menu-arrow icon-right-open"></i>' );
+			}
+
+		};
+		
+		
+		var doMenu = function(){
+
+			if( ( window.innerWidth >= options.mobileInit ) || ( ! options.responsive ) ){
+				
+				// desktop --------------------------------
+
+				$( '> li, ul:not(.hkd-megamenu) li', menu ).hover(function() {
+					
+					$(this).stop(true,true).addClass( options.hoverClass );
+					
+					$(this).children( 'ul' ).stop(true,true).fadeIn( options.delay );
+					
+					
+				}, function(){
+					
+					$(this).stop(true,true).removeClass( options.hoverClass );
+					
+					$(this).children( 'ul' ).stop(true,true).fadeOut( options.delay );	
+					
+				});
+				
+			} else {
+
+				// mobile ---------------------------------
+				
+				$( 'li', menu ).unbind('hover');
+				
+				$( 'li > .menu-toggle', menu ).off('click').on('click', function(){
+	
+					var el = $(this).closest('li');
+					
+					if( el.hasClass( options.hoverClass ) ){
+	
+						el.removeClass( options.hoverClass )
+							.children('ul').stop(true,true).fadeOut( options.delay );			
+						
+					} else {
+		
+						el.addClass( options.hoverClass )
+							.children('ul').stop(true,true).fadeIn( options.delay );	
+	
+					}
+					
+				});
+				
+			}
+			
+		};
+		
+
+		$(window).bind( 'resize', doMenu );
+		
+		var __constructor = function(){
+			init();
+			doMenu();
+		};
+		
+		__constructor();
+
+	};
+
+})(jQuery);
